9d9aeaad27125fc31f9b10ba669b853fc2ff7185
geisterfurz007 <geisterfurz007@gmail.com>
2018-08-18 15:30:53 +0200

    Added endpoints and method to edit textfile
    Added a POST, PUT and DELETE endpoint to modify the TextFile of a domain
    for a given type

--- src/main/java/org/sobotics/heatdetector/rest/domain/DomainController.java
+++ src/main/java/org/sobotics/heatdetector/rest/domain/DomainController.java
@@ -1,28 +1,45 @@
 package org.sobotics.heatdetector.rest.domain;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.sobotics.heatdetector.domain.DomainHandler;
 import org.sobotics.heatdetector.domain.file.TextFile;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 public class DomainController {
-
+	
 	@GetMapping("heatdetector/api/domains/**")
-	public List<String> getDomains(final HttpServletRequest request)  {
+	public List<String> getDomains(final HttpServletRequest request) {
 		//TODO: Show only domains that users have access to
 		return DomainHandler.getInstance().getDomainNames();
 	}
 	
 	@GetMapping("heatdetector/api/regex/{domain}/**")
-	public TextFile getRegexen(final HttpServletRequest request,@PathVariable("domain") String domain, @RequestParam(value = "type", required = true) int type) {
+	public TextFile getRegexen(final HttpServletRequest request, @PathVariable("domain") String domain, @RequestParam(value = "type", required = true) int type) {
 		return DomainHandler.getInstance().getDomain(domain).getRegexen(type).getFile();
+	}
+	
+	@PostMapping("heatdetector/api/regex/{domain}/**")
+	public void addRegex(final HttpServletRequest request, @PathVariable("domain") String domain, @RequestParam(value = "type", required = true) int type, @RequestParam(value = "regex", required = true) String regex, @RequestParam(value = "index", required = false) Integer index) {
+		TextFile file = DomainHandler.getInstance().getDomain(domain).getRegexen(type).getFile();
+		if (index != null)
+			file.addLine(index, regex);
+		else
+			file.addLine(regex);
+	}
+	
+	@PutMapping("heatdetector/api/regex/{domain}/**")
+	public void editRegex(final HttpServletRequest request, @PathVariable("domain") String domain, @RequestParam(value = "type", required = true) int type, @RequestParam(value = "regex", required = true) String regex, @RequestParam(value = "index", required = true) int index) {
+		TextFile file = DomainHandler.getInstance().getDomain(domain).getRegexen(type).getFile();
+		file.editLine(index, regex);
+	}
+	
+	@DeleteMapping("heatdetector/api/regex/{domain}/**")
+	public void deleteRegex(final HttpServletRequest request, @PathVariable("domain") String domain, @RequestParam(value = "type", required = true) int type, @RequestParam(value = "index", required = true) int index) {
+		TextFile file = DomainHandler.getInstance().getDomain(domain).getRegexen(type).getFile();
+		file.deleteLine(index);
 	}
 	
 }
